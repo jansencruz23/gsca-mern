c03cee7664dae8667becd19f52fe9af6f9adeef9
Jansen Cruz <jansencruz12345@gmail.com>
2025-11-23 03:33:35 +0800

    Update CORS policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,13 +20,7 @@ const app = express();
 // Middleware
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: allowedOrigins,
         credentials: true,
     })
 );
